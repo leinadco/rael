1739d0d0848680e7e5dc71a1bdbe66328e29a354
Dumitru <cozmiucd.1at@gmail.com>
2025-04-07 23:05:51 +0300

    responsive updates

--- footer.js
+++ footer.js
@@ -1,7 +1,6 @@
 export function renderFooter(div) {
     let presentation =
         "<h1 class='footer'>All rights reserved &copy; 2022</h1>";
-    presentation += "<a href='addImages.html'>Add Images</a>";
     div.innerHTML = presentation;
     return div;
 }
